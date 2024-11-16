654934d65d93dbb7ab5ea9b35c0705068c7ddb3a
Nilesh Ranjan <nileshranjan2005@gmail.com>
2024-11-16 15:36:01 +0530

    Finalized the whole backend.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import plotly.express as pl
+from backend import get_data
 
+# Added the basic frontend text
 st.title('Weather Forecast')
 place = st.text_input("Place: ")
 days = st.slider("Days to Forecast", min_value=1, max_value=5, help="Number of days to be forecasted for a given "
@@ -8,15 +10,19 @@ days = st.slider("Days to Forecast", min_value=1, max_value=5, help="Number of d
 option = st.selectbox('Select mode of view', ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
+if place:
+    # Get Temperature/sky data
+    filtered_data = get_data(place, days)
+    # Plot the data accordingly
+    if option == "Temperature":
+        temp = [dict["main"]["temp"] for dict in filtered_data]
+        dates =[dict["dt_txt"] for dict in filtered_data]
+        graph = pl.line(x=dates, y=temp, labels={"x": "Date", "y": "Temperature (C)"})
+        st.plotly_chart(graph)
 
-def get_data(days):
-    date = ["2024-11-15", "2024-11-16", "2024-11-17"]
-    temperature = [11, 13, 16]
-    temperature = [days * i for i in temperature]
-    return date , temperature
-
-
-d, t = get_data(days)
-
-graph = pl.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
-st.plotly_chart(graph)
+    if option == "Sky":
+        images = {"Clear": "images/clear.png", "Clouds":"images/cloud.png", "Snow": "images/snow.png",
+                  "Rain": "images/rain.png"}
+        sky_condition = [dict["weather"][0]["main"] for dict in filtered_data]
+        image_path = [images[condition] for condition in sky_condition]
+        st.image(image_path, width=100)
